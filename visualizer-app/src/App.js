2b5ef786b8fd986a3d729eacabac92f2ec9267e6
Justin Levy <justinlevy20@gmail.com>
2022-07-07 18:18:25 -0400

    Base site created

--- visualizer-app/src/App.js
+++ visualizer-app/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>
+        VisualizeIt
+      </h1>
+      
+
+      <div className='content'>
+        <div className='algoGrid'>
+
+          <div className='gridCard'>
+            <h1>Sorting</h1>
+          </div>
+
+          <div className='gridCard'>
+            <h1>Pathfinding</h1>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
